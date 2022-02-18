2f1e6c2ee36ad1889773cf6af6d16f8bc209aacb
mikkelfo <mikk528a@gmail.com>
2022-02-18 11:51:47 +0100

    Removed CPU offloading

--- src/models/pl_main.py
+++ src/models/pl_main.py
@@ -4,5 +4,5 @@ from data_module import DataModule
 
 model = ProphetNetModule()
 dm = DataModule()
-trainer = Trainer(gpus=1, accelerator="gpu", strategy="deepspeed_stage_3_offload", precision=16)
+trainer = Trainer(gpus=1, accelerator="gpu", strategy="deepspeed_stage_3", precision=16)
 trainer.fit(model, dm)
