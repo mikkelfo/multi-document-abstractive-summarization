d9621a0d852e23cccb46ecf71f64d3d6c6a62e61
mikkelfo <mikk528a@gmail.com>
2022-03-03 17:33:58 +0100

    Added danish implementation

--- src/models/main_da.py
+++ src/models/main_da.py
@@ -1,29 +1,26 @@
 import torch
-from transformers import ProphetNetForConditionalGeneration
 import os
-from utils import process_chunk
+from utils import process_chunk, process_chunk_da
 import wandb
+from torch.cuda.amp import autocast
+from model import ProphetNetAutocast
 
 ''' CONSTANTS '''
 BATCH_SIZE = 4
 EPOCHS = 10
 TOKEN_LENGTH = 400
 GRADIENT_ACCUMULATION_STEPS = 16
-N_CHUNKS = len(os.listdir('data/processed/tokenized/cnn-dm/summary'))
+N_CHUNKS = len(os.listdir('data/processed/tokenized/danewsroom/abstractive/summary'))
 
 
 ''' INITIALIZATION '''
-model = ProphetNetForConditionalGeneration.from_pretrained('microsoft/prophetnet-large-uncased')
-model = torch.nn.DataParallel(model)
-model.to('cuda')
-for param in list(model.parameters())[:-1]:
-    param.requires_grad = False
+model = ProphetNetAutocast()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
 
 # For model checkpointing
-if not os.path.isdir('checkpoints'):
-    os.mkdir('checkpoints')
-run_num = len(os.listdir('checkpoints'))
+if not os.path.isdir('checkpoints_da'):
+    os.mkdir('checkpoints_da')
+run_num = len(os.listdir('checkpoints_da'))
 
 ''' WANDB '''
 wandb.init(project="abstractive-summarization", entity="mikkelfo")
@@ -36,17 +33,19 @@ for epoch in range(EPOCHS):
     # for each file
     for chunk_idx in range(N_CHUNKS):
         chunk_loss = 0
-        for batch_idx, batch in enumerate(process_chunk(chunk_idx, TOKEN_LENGTH, BATCH_SIZE)):
+        for batch_idx, batch in enumerate(process_chunk_da(chunk_idx, TOKEN_LENGTH, BATCH_SIZE)):
             input_ids, attention_mask, labels = batch
-            loss = model(input_ids=input_ids, attention_mask = attention_mask, labels = labels).loss.sum()
-            loss.backward()
+
+            with autocast():
+                loss = model(input_ids=input_ids, attention_mask = attention_mask, labels = labels)
+                loss.backward()
 
             # Gradient accumulation
             if batch_idx % GRADIENT_ACCUMULATION_STEPS == 0 and batch_idx != 0:
                 optimizer.step()
                 optimizer.zero_grad(set_to_none=True)
 
-            chunk_loss = loss.item()
+            chunk_loss = loss.detach()
         optimizer.step()
         optimizer.zero_grad(set_to_none=True)
 
@@ -60,4 +59,4 @@ for epoch in range(EPOCHS):
     print(f'----------     Epoch {epoch} loss: {epoch_loss}     ----------')
     print()
 
-    torch.save(model.state_dict(), f'checkpoints/run-{run_num}_epoch{epoch}')
+    torch.save(model.state_dict(), f'checkpoints_da/run-{run_num}_epoch{epoch}')
--- src/models/utils.py
+++ src/models/utils.py
@@ -1,8 +1,21 @@
 import torch
 
 def process_chunk(chunk_idx, token_length, batch_size):
-    summary = torch.load(f'data/processed/tokenized/summary/chunk_{chunk_idx}.pt')
-    text = torch.load(f'data/processed/tokenized/text/chunk_{chunk_idx}.pt')
+    summary = torch.load(f'data/processed/tokenized/cnn-dm/summary/chunk_{chunk_idx}.pt')
+    text = torch.load(f'data/processed/tokenized/cnn-dm/text/chunk_{chunk_idx}.pt')
+
+    input_ids, attention_mask, = text['input_ids'][:, :token_length].to('cuda'), text['attention_mask'][:, :token_length].to('cuda')
+    decoder_input_ids, _ = summary['input_ids'][:, :token_length].to('cuda'), summary['attention_mask'][:, :token_length].to('cuda')
+
+    N = len(input_ids)  # Since it's not exactly 1000 due to the discarded stories
+    for i in range(0, N, batch_size):
+        batch = input_ids[i:(i+batch_size)], attention_mask[i:(i+batch_size)], decoder_input_ids[i:(i+batch_size)]
+        yield batch
+
+
+def process_chunk_da(chunk_idx, token_length, batch_size):
+    summary = torch.load(f'data/processed/tokenized/danewsroom/abstractive/summary/chunk_{chunk_idx}.pt')
+    text = torch.load(f'data/processed/tokenized/danewsroom/abstractivetext/chunk_{chunk_idx}.pt')
 
     input_ids, attention_mask, = text['input_ids'][:, :token_length], text['attention_mask'][:, :token_length]
     decoder_input_ids, _ = summary['input_ids'][:, :token_length], summary['attention_mask'][:, :token_length]
