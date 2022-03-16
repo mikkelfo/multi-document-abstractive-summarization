aef10974d36cfdb492e92634de1394baf95486a8
mikkelfo <mikk528a@gmail.com>
2022-03-16 08:31:06 +0100

    Updated checkpointing

--- src/models/main.py
+++ src/models/main.py
@@ -22,9 +22,9 @@ optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, weight_decay=0.01)
 # For model checkpointing
 if not os.path.isdir('checkpoints'):
     os.mkdir('checkpoints')
-    run_num = next(os.walk('checkpoints'))[1]
-    if not os.path.isdir(f'checkpoints/{run_num}'):
-        os.mkdir(f'checkpoints/{run_num}')
+run_num = next(os.walk('checkpoints'))[1]
+if not os.path.isdir(f'checkpoints/{run_num}'):
+    os.mkdir(f'checkpoints/{run_num}')
 
 ''' WANDB '''
 wandb.init(project="abstractive-summarization-runs", entity="mikkelfo")
