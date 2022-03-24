b2b17072b262c947e32f1c4dc13b674145da3fed
mikkelfo <mikk528a@gmail.com>
2022-03-24 22:37:44 +0100

    WCEP MDS implementation

--- src/models/model.py
+++ src/models/model.py
@@ -1,10 +1,11 @@
 import torch
-from transformers import ProphetNetForConditionalGeneration
+from transformers import ProphetNetForConditionalGeneration, ProphetNetEncoder, ProphetNetDecoder, ProphetNetForCausalLM
 from torch.cuda.amp import autocast
+import torch.nn.functional as F 
 
 class ProphetNetAutocast(torch.nn.Module):
     def __init__(self, freeze_layers=True) -> None:
-        super().__init__()
+        super(ProphetNetAutocast, self).__init__()
         self.model = ProphetNetForConditionalGeneration.from_pretrained('microsoft/prophetnet-large-uncased')
         self.model = torch.nn.DataParallel(self.model)
         self.model.to('cuda')
@@ -19,3 +20,43 @@ class ProphetNetAutocast(torch.nn.Module):
         x = self.model(input_ids=input_ids, attention_mask=attention_mask, labels=labels, use_cache=False)
         return x.loss.sum()
 
+
+class ProphetNetMulti(torch.nn.Module):
+    def __init__(self) -> None:
+        super(ProphetNetMulti, self).__init__()
+        self.encoder = ProphetNetEncoder.from_pretrained('patrickvonplaten/prophetnet-large-uncased-standalone')
+        self.model = ProphetNetForCausalLM.from_pretrained('microsoft/prophetnet-large-uncased')
+        assert self.model.config.is_decoder, f"{self.model.__class__} has to be configured as a decoder."
+
+    def get_params(self):
+        params = list(self.encoder.parameters()) + list(self.model.parameters())
+        return params
+
+    def encoder_pass(self, input_ids, attention_mask):
+        last_hidden = self.encoder(input_ids=input_ids, attention_mask=attention_mask).last_hidden_state
+
+        return last_hidden
+
+    def decoder_pass(self, input_ids, attention_mask, encoder_hidden_states):
+        logits = self.model(input_ids=input_ids, attention_mask=attention_mask, encoder_hidden_states=encoder_hidden_states, use_cache=False).logits
+
+        return logits
+
+    # Taken from https://github.com/huggingface/transformers/blob/v4.17.0/src/transformers/models/prophetnet/modeling_prophetnet.py#L2256
+    @autocast()
+    def forward(self, input_ids, attention_mask, target):
+        last_hidden = self.encoder(input_ids=input_ids, attention_mask=attention_mask).last_hidden_state
+        
+        logits = self.model(input_ids=target, encoder_hidden_states=last_hidden, use_cache=False).logits
+        lprobs = F.log_softmax(logits[0, :, :], dim=-1)
+
+        loss = F.nll_loss(lprobs, target)
+        return loss
+
+if __name__ == '__main__':
+    model = ProphetNetMulti()
+    model.train()
+    input_ids = torch.randint(1, 3000, (10, 100))
+    attention_mask = torch.ones(10, 100)
+    labels = torch.randint(1, 3000, (1, 100))
+    model(input_ids, attention_mask, labels)
--- src/models/utils.py
+++ src/models/utils.py
@@ -1,6 +1,9 @@
 import torch
 from torch.cuda.amp import autocast
 import os
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import gzip, json
 
 
 def process_chunk(chunk_idx, token_length, batch_size, split):
@@ -56,3 +59,49 @@ def validate_da(model, TOKEN_LENGTH, BATCH_SIZE):
     model.train()
     return val_loss
 
+def validate_wcep(model, TOKEN_LENGTH, CLUSTER_SIZE):
+    val_targets = target_summaries('val')
+    N_CLUSTERS_VAL = len(os.listdir('data/processed/wcep/text/validation'))
+    val_loss = 0
+    model.eval()
+    with torch.no_grad():
+        for idx in range(N_CLUSTERS_VAL):
+            cluster = torch.load(f'data/processed/wcep/text/validation/cluster_{idx}.pt')
+            input_ids = cluster.input_ids[:CLUSTER_SIZE, :TOKEN_LENGTH]
+            attention_mask = cluster.attention_mask[:CLUSTER_SIZE, :TOKEN_LENGTH]
+            target = input_ids[val_targets[idx]]
+            with autocast():
+                loss = model(input_ids=input_ids, attention_mask=attention_mask, target=target)
+            val_loss += loss.detach() / len(input_ids)
+    model.train()
+    return val_loss
+
+def read_jsonl_gz(path):
+    with gzip.open(path) as f:
+        for l in f:
+            yield json.loads(l)
+
+
+def target_summaries(split):
+    vectorizer = TfidfVectorizer()
+    data = list(read_jsonl_gz(f'data/raw/wcep/{split}.jsonl.gz'))
+
+    targets = []
+    for cluster in data:
+        articles = cluster['articles']
+        text = [a['text'] for a in articles][:16]
+        X = vectorizer.fit_transform(text)
+        targets.append(cosine_similarity(X).sum(1).argmax())
+
+    return targets
+
+
+def concat_chunks(dir):
+    input_ids = torch.tensor([])
+    attention_mask = torch.tensor([])
+    for i in range(len(os.listdir(dir))):
+        chunk = torch.load(f'{dir}/chunk_{i}.pt')
+        input_ids = torch.cat((input_ids, chunk.input_ids))
+        attention_mask = torch.cat((attention_mask, chunk.attention_mask))
+
+    return list(zip(input_ids, attention_mask))
