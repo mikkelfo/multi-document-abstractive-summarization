8cad435c55788112dd95d82423a536b237eead97
mikkelfo <mikk528a@gmail.com>
2022-03-30 09:49:58 +0200

    Fixed forward loop

--- src/models/model.py
+++ src/models/model.py
@@ -25,8 +25,8 @@ class ProphetNetAutocast(torch.nn.Module):
                 param.requires_grad = False
 
     @autocast()
-    def forward(self, input_ids, attention_mask, target, target_mask):
-        x = self.model(input_ids=input_ids, attention_mask=attention_mask, decoder_input_ids=target, decoder_attention_mask=target_mask, use_cache=False)
+    def forward(self, input_ids, attention_mask, decoder_input_ids, decoder_attention_mask):
+        x = self.model(input_ids=input_ids, attention_mask=attention_mask, decoder_input_ids=decoder_input_ids, decoder_attention_mask=decoder_attention_mask, use_cache=False)
         return x.loss.sum()
 
 
