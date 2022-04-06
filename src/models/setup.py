b9018962577d6e5d1bcbce0434eaa961dc4b1a30
mikkelfo <mikk528a@gmail.com>
2022-04-06 14:02:48 +0200

    Fixed setup and added print statements

--- src/models/setup.py
+++ src/models/setup.py
@@ -11,7 +11,7 @@ def setup():
     parser.add_argument('--gpus', default=1, type=int, help="Number of GPUs to utilise")
     parser.add_argument('--epochs', default=10, type=int, help="Number of epochs")
     parser.add_argument('--batch_size', default=1, type=int, help="Micro-batch size")
-    parser.add_argument('--tokens', default=256, type=int, help="Number of tokens")
+    parser.add_argument('--token_length', default=256, type=int, help="Number of tokens")
     parser.add_argument('--checkpointing', default=50, type=int, help="How often the model is saved ")
     parser.add_argument('--dir', type=str, help="Change the directory")
 
@@ -19,7 +19,7 @@ def setup():
 
     assert args.epochs > 0
     assert args.batch_size > 0 and args.batch_size <= 512
-    assert args.tokens > 0 and args.tokens < 512
+    assert args.token_length > 0 and args.token_length < 512
     assert args.checkpointing > 0
     assert args.gpus >= 0
     assert args.lang == 'da' or args.lang == 'en', "Only supports 'da' or 'en'"
@@ -34,14 +34,18 @@ def setup():
     args.gradient_accumulation_step = 512 / args.batch_size
     
     if args.xlm:
+        print("Initializing XLM model")
         model = XLMProphetNetForConditionalGeneration.from_pretrained("microsoft/xprophetnet-large-wiki100-cased")
     else:
+        print("Initializing ProphetNet model")
         model = ProphetNetForConditionalGeneration.from_pretrained('microsoft/prophetnet-large-uncased')
     model.gradient_checkpointing_enable()
     if args.gpus > 0:
         model.to('cuda')
+        print("Running on GPU")
         if args.gpus > 1:
             model = torch.nn.DataParallel(model)
+            print("DataParallel activated")
     
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, weight_decay=0.01)
 
@@ -50,6 +54,7 @@ def setup():
             args.dir = 'danewsroom/abstractive'
         elif args.lang == 'en':
             args.dir = 'cnn-dm'
+    print("Directory:", args.dir)
 
     wandb.init(project="abstractive-summarization-runs", entity="mikkelfo")
     wandb.watch(model)
