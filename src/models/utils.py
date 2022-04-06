08d1acccd0a939c33dc76a9dc9b46d5286ecb5da
mikkelfo <mikk528a@gmail.com>
2022-04-06 14:27:10 +0200

    Added cuda call to batch instead of chunk (saves memory)

--- src/models/utils.py
+++ src/models/utils.py
@@ -4,15 +4,15 @@ import gzip, json
 
 
 def process_chunk(split, chunk_idx, args):
-    summary = torch.load(f'data/processed/{args.dir}/summary/{split}/chunk_{chunk_idx}.pt').to('cuda')
-    text = torch.load(f'data/processed/{args.dir}/text/{split}/chunk_{chunk_idx}.pt').to('cuda')
+    summary = torch.load(f'data/processed/{args.dir}/summary/{split}/chunk_{chunk_idx}.pt')
+    text = torch.load(f'data/processed/{args.dir}/text/{split}/chunk_{chunk_idx}.pt')
 
     input_ids, attention_mask, = text['input_ids'][:, :args.token_length], text['attention_mask'][:, :args.token_length]
     decoder_input_ids, _ = summary['input_ids'][:, :args.token_length], summary['attention_mask'][:, :args.token_length]
 
     N = len(input_ids)  # 1024
     for i in range(0, N, args.batch_size):
-        batch = input_ids[i:(i+args.batch_size)], attention_mask[i:(i+args.batch_size)], decoder_input_ids[i:(i+args.batch_size)]
+        batch = input_ids[i:(i+args.batch_size)].to('cuda'), attention_mask[i:(i+args.batch_size)].to('cuda'), decoder_input_ids[i:(i+args.batch_size)].to('cuda')
         yield batch
 
 
