d31278b6d68bac0c5dfe53d497cae01ed768471f
mikkelfo <mikk528a@gmail.com>
2022-04-25 09:53:38 +0200

    Updated generation pipeline

--- src/models/generation.py
+++ src/models/generation.py
@@ -1,8 +1,54 @@
 import os
 import torch
-from transformers import ProphetNetForConditionalGeneration
-from transformers import ProphetNetTokenizer
+from transformers import ProphetNetForConditionalGeneration, XLMProphetNetForConditionalGeneration
+from transformers import ProphetNetTokenizer, XLMProphetNetTokenizer
 import json
+from utils import process_chunk
+import argparse
+
+def setup():
+    parser = argparse.ArgumentParser(description='Training script for SDS ProphetNet')
+    parser.add_argument('dir', type=str)
+    parser.add_argument('checkpoints', type=str)
+    parser.add_argument('--xlm', const=True, default=False, nargs="?", help="Use XLMProphetNet (Default: ProphetNet)")
+    parser.add_argument('--mds', const=True, default=False, nargs="?", help="Activate MDS setup")
+    parser.add_argument('--batch_size', default=1, type=int, help="Micro-batch size")
+    parser.add_argument('--token_length', default=256, type=int, help="Number of tokens")
+
+    args = parser.parse_args()
+
+    if args.xlm:
+        tokenizer = XLMProphetNetTokenizer.from_pretrained("microsoft/xprophetnet-large-wiki100-cased")
+        model = XLMProphetNetForConditionalGeneration.from_pretrained("microsoft/xprophetnet-large-wiki100-cased")
+    else:
+        tokenizer = ProphetNetTokenizer.from_pretrained('microsoft/prophetnet-large-uncased')
+        model = ProphetNetForConditionalGeneration.from_pretrained('microsoft/prophetnet-large-uncased')
+
+    model.to('cuda')
+
+    return model, tokenizer, args
+
+def generate_summaries():
+    model, tokenizer, args = setup()
+    os.mkdir(f'summaries/{args.checkpoints}')
+    N_chunks = len(os.listdir(args.dir + '/summary/test'))
+    for checkpoint in os.listdir(args.checkpoints):
+        if 'end' not in checkpoint:
+            continue
+        print("Starting:", checkpoint)
+        dic = torch.load(f'{args.checkpoints}/{checkpoint}')
+        model.load_state_dict(dic)
+
+        summaries = []
+        for chunk_idx in range(N_chunks):
+            for batch in process_chunk('test', chunk_idx, args):
+                input_ids, attention_mask, _ = batch
+                output = model.generate(input_ids=input_ids, attention_mask=attention_mask, min_length=45, max_length=110, num_beams=5, no_repeat_ngram_size=3, length_penalty=1.2)
+                gen_summary = tokenizer.batch_decode(output, skip_special_tokens=True)
+                summaries += gen_summary
+        with open(f'summaries/{args.checkpoints}/{checkpoint}.json', 'w') as file:
+            json.dump(summaries, file, indent=4)
+
 
 def generate_summaries(model_dir, data_dir='data/processed/cnn-dm/text/test'):
     model = ProphetNetForConditionalGeneration.from_pretrained('microsoft/prophetnet-large-uncased')
@@ -63,5 +109,4 @@ def clean_dic(dic):
 
 
 if __name__ == '__main__':
-    # generate_summaries('models/unfrozen-cnn')
-    generate_original()
+    generate_summaries()
