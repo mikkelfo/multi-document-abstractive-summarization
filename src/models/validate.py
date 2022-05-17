befb953f73b3cd9832773f046d5a1db5ba7ca8b0
mikkelfo <mikk528a@gmail.com>
2022-05-17 21:26:26 +0200

    Removed unsupervised training

--- src/models/validate.py
+++ src/models/validate.py
@@ -16,12 +16,6 @@ def validate(model, args):
 
             for batch_idx, batch in enumerate(process_chunk('validation', chunk_idx, args)):
                 input_ids, attention_mask, labels = batch
-                if args.unsupervised:
-                    target_index = next(targets)
-                    labels = input_ids[target_index].unsqueeze(0)                                   # Assign labels to the target summary
-                    labels = labels.masked_fill_(labels == 0, -100)                                 # Remove padding (Can be done because batch size is always 1)
-                    input_ids = torch.cat((input_ids[:target_index], input_ids[target_index+1:]))   # Remove target summary from input
-                    attention_mask = torch.cat((attention_mask[:target_index], attention_mask[target_index+1:]))   # Remove target summary from mask
                 with autocast():
                     if args.mds:
                         loss = custom_forward_mds(model, input_ids, attention_mask, labels, args).loss.mean()
