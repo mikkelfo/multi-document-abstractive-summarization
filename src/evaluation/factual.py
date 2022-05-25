34f56451213e630858ff5fc264fd87448bed8340
mikkelfo <mikk528a@gmail.com>
2022-05-25 13:23:55 +0200

    added NERO for cnn

--- src/evaluation/factual.py
+++ src/evaluation/factual.py
@@ -4,7 +4,39 @@ import spacy
 def lowercase(array):
     return [el.text.lower() for el in array]
 
-def NER_overlap(pipeline):
+def NER_overlap_cnn(pipeline):
+    with open('summaries/best/cnn-dm.json', 'r') as f:
+        hypothesis = json.load(f)
+    hypothesis = sum(hypothesis, [])
+    with open('summaries/best/cnn-dm-xlm.json', 'r') as f:
+        hypothesis_xlm = json.load(f)
+    hypothesis_xlm = sum(hypothesis_xlm, [])
+    with open('data/processed/cnn-dm/test.json', 'r') as f:
+        data = json.load(f)
+    text, reference = zip(*data)
+
+    nlp = spacy.load(pipeline, exclude=["tagger", "parser", "attribute_ruler", "lemmatizer"])
+
+    correct_hyp, correct_xlm, correct_ref  = 0, 0, 0
+    for i in range(len(hypothesis)):
+        doc_hyp = lowercase(nlp(hypothesis[i]).ents)
+        doc_xlm = lowercase(nlp(hypothesis_xlm[i]).ents)
+        doc_ref = lowercase(nlp(reference[i]).ents)
+        doc_text = lowercase((nlp(text[i]).ents))
+
+        count_hyp = [1 if ent in doc_text else 0 for ent in doc_hyp]
+        count_xlm = [1 if ent in doc_text else 0 for ent in doc_xlm]
+        count_ref = [1 if ent in doc_text else 0 for ent in doc_ref]
+
+        correct_hyp += sum(count_hyp) / len(count_hyp) if len(count_hyp) > 0 else 0
+        correct_xlm += sum(count_xlm) / len(count_xlm) if len(count_xlm) > 0 else 0
+        correct_ref += sum(count_ref) / len(count_ref) if len(count_ref) > 0 else 0
+
+    print(correct_hyp / len(hypothesis))
+    print(correct_xlm / len(hypothesis))
+    print(correct_ref / len(hypothesis))
+
+def NER_overlap_wcep(pipeline):
     with open('summaries/best/wcep-mean.json', 'r') as f:
         hypothesis = json.load(f)
     hypothesis = sum(sum(hypothesis, []), [])
@@ -39,6 +71,6 @@ def NER_overlap(pipeline):
 
 
 if __name__ == '__main__':
-    NER_overlap('en_core_web_sm')
+    NER_overlap_cnn('en_core_web_sm')
     print()
-    NER_overlap('en_core_web_trf')
+    NER_overlap_cnn('en_core_web_trf')
