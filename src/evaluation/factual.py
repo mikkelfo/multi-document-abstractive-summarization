ea428be641ad01a640454aed0b5fde648098b9a1
mikkelfo <mikk528a@gmail.com>
2022-05-26 14:25:01 +0200

    Updated NERO pipeline

--- src/evaluation/factual.py
+++ src/evaluation/factual.py
@@ -4,73 +4,61 @@ import spacy
 def lowercase(array):
     return [el.text.lower() for el in array]
 
-def NER_overlap_cnn(pipeline):
-    with open('summaries/best/cnn-dm.json', 'r') as f:
-        hypothesis = json.load(f)
-    hypothesis = sum(hypothesis, [])
-    with open('summaries/best/cnn-dm-xlm.json', 'r') as f:
-        hypothesis_xlm = json.load(f)
-    hypothesis_xlm = sum(hypothesis_xlm, [])
-    with open('data/processed/cnn-dm/test.json', 'r') as f:
-        data = json.load(f)
-    text, reference = zip(*data)
+def get_summaries(*summaries):
+    extracted = []
+    for summ in summaries:
+        with open(f'summaries/best/{summ}.json', 'r') as f:
+            data = json.load(f)
+        if 'wcep' in summ:
+            data = sum(sum(data, []), [])
+        else:
+            data = sum(data, [])
+        extracted.append(data)
+    return extracted
 
+def get_original(file):
+    with open(f'data/processed/{file}/test.json', 'r') as f:
+        data = json.load(f)
+    if file == 'wcep':
+        summary, text = zip(*data)
+    elif file == 'cnn-dm':
+        text, summary = zip(*data)
+    return text, summary
+
+def NER_overlap(pipeline, text, reference, summaries):
     nlp = spacy.load(pipeline, exclude=["tagger", "parser", "attribute_ruler", "lemmatizer"])
 
-    correct_hyp, correct_xlm, correct_ref  = 0, 0, 0
-    for i in range(len(hypothesis)):
-        doc_hyp = lowercase(nlp(hypothesis[i]).ents)
-        doc_xlm = lowercase(nlp(hypothesis_xlm[i]).ents)
-        doc_ref = lowercase(nlp(reference[i]).ents)
-        doc_text = lowercase((nlp(text[i]).ents))
+    summaries = [reference] + summaries
 
-        count_hyp = [1 if ent in doc_text else 0 for ent in doc_hyp]
-        count_xlm = [1 if ent in doc_text else 0 for ent in doc_xlm]
-        count_ref = [1 if ent in doc_text else 0 for ent in doc_ref]
+    overlap = [0] * len(summaries)
+    ent_count = [0] * len(summaries)
+    for i, t in enumerate(text):
+        if i in [len(summ) for summ in summaries]:
+            print("Stopped early")
+            print([len(summ) for summ in summaries])
+            break
+        if type(t) == str:
+            text_ents = lowercase(nlp(t).ents)
+        elif type(t) == list:
+            text_ents = lowercase(sum([list(nlp(x).ents) for x in t], []))
 
-        correct_hyp += sum(count_hyp) / len(count_hyp) if len(count_hyp) > 0 else 0
-        correct_xlm += sum(count_xlm) / len(count_xlm) if len(count_xlm) > 0 else 0
-        correct_ref += sum(count_ref) / len(count_ref) if len(count_ref) > 0 else 0
+        for j, summ in enumerate(summaries):
+            summ_ents = lowercase(nlp(summ[i]).ents)
+            count = [1 if ent in text_ents else 0 for ent in summ_ents]
+            if len(count) > 0:
+                overlap[j] += sum(count) / len(count)
+                ent_count[j] += len(count)
 
-    print(correct_hyp / len(hypothesis))
-    print(correct_xlm / len(hypothesis))
-    print(correct_ref / len(hypothesis))
-
-def NER_overlap_wcep(pipeline):
-    with open('summaries/best/wcep-mean.json', 'r') as f:
-        hypothesis = json.load(f)
-    hypothesis = sum(sum(hypothesis, []), [])
-    with open('summaries/best/wcep-serial.json', 'r') as f:
-        hypothesis_serial = json.load(f)
-    hypothesis_serial = sum(sum(hypothesis_serial, []), [])
-    with open('data/processed/wcep/test.json', 'r') as f:
-        data = json.load(f)
-    reference, text = zip(*data)
-
-
-    nlp = spacy.load(pipeline, exclude=["tagger", "parser", "attribute_ruler", "lemmatizer"])
-
-    correct_hyp, correct_ref, correct_serial = 0, 0, 0
-    for i in range(len(hypothesis)):
-        doc_hyp = lowercase(nlp(hypothesis[i]).ents)
-        doc_serial = lowercase(nlp(hypothesis_serial[i]).ents)
-        doc_ref = lowercase(nlp(reference[i]).ents)
-        doc_text = lowercase(sum([list(nlp(t).ents) for t in text[i]], []))
-
-        count_hyp = [1 if ent in doc_text else 0 for ent in doc_hyp]
-        count_ref = [1 if ent in doc_text else 0 for ent in doc_ref]
-        count_serial = [1 if ent in doc_text else 0 for ent in doc_serial]
-
-        correct_hyp += sum(count_hyp) / len(count_hyp) if len(count_hyp) > 0 else 0
-        correct_serial += sum(count_serial) / len(count_serial) if len(count_serial) > 0 else 0
-        correct_ref += sum(count_ref) / len(count_ref) if len(count_ref) > 0 else 0
-
-    print(correct_hyp / len(hypothesis))
-    print(correct_serial / len(hypothesis))
-    print(correct_ref / len(hypothesis))
+    for i in range(len(summaries)):
+        print(overlap[i] / len(text), ent_count[i])
 
 
 if __name__ == '__main__':
-    NER_overlap_cnn('en_core_web_sm')
+    hypotheses = get_summaries('cnn-dm', 'cnn-dm-xlm')
+    text, reference = get_original('cnn-dm')
+    NER_overlap('en_core_web_trf', text, reference, hypotheses)
     print()
-    NER_overlap_cnn('en_core_web_trf')
+
+    hypotheses = get_summaries('wcep-mean', 'wcep-serial')
+    text, reference = get_original('wcep')
+    NER_overlap('en_core_web_trf', text, reference, hypotheses)
