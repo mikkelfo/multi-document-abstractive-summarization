00dd27a2d6b70c207d53564240c6283de2f997d5
mikkelfo <mikk528a@gmail.com>
2022-06-03 10:23:53 +0200

    Updated LR Scheduler

--- src/models/scheduler.py
+++ src/models/scheduler.py
@@ -2,44 +2,36 @@ from torch.optim.lr_scheduler import _LRScheduler
 from torch.optim import Adam
 from transformers import ProphetNetForConditionalGeneration
 
-# https://github.com/pytorch/fairseq/blob/main/fairseq/optim/lr_scheduler/inverse_square_root_schedule.py
 class InverseSqrtScheduler(_LRScheduler):
-    def __init__(self, optimizer, warmup=1000, warmup_init_lr=1e-07, end_lr=1e-4) -> None:
+    def __init__(self, optimizer, warmup_updates=1000, warmup_init_lr=1e-07, warmup_end_lr=1e-4) -> None:
         self.optimizer = optimizer
-        self._step = 0
-        self.warmup = warmup
-        self.end_lr = end_lr
+
+        self.warmup_updates = warmup_updates
         self.warmup_init_lr = warmup_init_lr
+        self.warmup_end_lr = warmup_end_lr
 
-        self.lr_step = (end_lr - warmup_init_lr) / warmup
-        self.decay_factor = end_lr * warmup**0.5
-
-        # Initial learning rate
+        self.decay_factor = warmup_end_lr * warmup_updates**0.5
         self.lr = warmup_init_lr
-        self.set_lr()
+        self.lr_step = (warmup_end_lr - warmup_init_lr) / warmup_updates
 
-    def step(self):
-        self._step += 1
-        self.step_update()
-        self.set_lr()
-        
-    def step_update(self):
-        if self._step < self.warmup:
-            self.lr = self.warmup_init_lr + self._step * self.lr_step
+        super(InverseSqrtScheduler, self).__init__(optimizer)
+
+    # https://github.com/pytorch/fairseq/blob/main/fairseq/optim/lr_scheduler/inverse_square_root_schedule.py
+    def get_lr(self):
+        num_updates = self._step_count - 1
+        if num_updates < self.warmup_updates:
+            self.lr = self.warmup_init_lr + num_updates * self.lr_step
         else:
-            self.lr = self.decay_factor * self._step**-0.5
-
-    def set_lr(self):
-        for param_group in self.optimizer.param_groups:
-            param_group['lr'] = self.lr
-        
-import matplotlib.pyplot as plt
+            self.lr = self.decay_factor * num_updates**-0.5
+        return [self.lr for group in self.optimizer.param_groups]
 
 
 if __name__ == '__main__':
+    import matplotlib.pyplot as plt
     model = ProphetNetForConditionalGeneration.from_pretrained('microsoft/prophetnet-large-uncased')
-    optimizer = Adam(model.parameters(), lr=1e-4)
+    optimizer = Adam(model.parameters(), weight_decay=0.01)
     scheduler = InverseSqrtScheduler(optimizer)
+
     x = []
     for i in range(5000):
         optimizer.step()
